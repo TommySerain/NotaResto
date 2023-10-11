f28bccd73e02968bfc0f4fd1d0ce88211dfa3c4c
Tommy Serain <tommy.serain@cc.in2p3.fr>
2023-10-11 12:54:43 +0200

    création des entités

---
+++ src/Entity/Review.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReviewRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ReviewRepository::class)]
+class Review
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $comment = null;
+
+    #[ORM\Column]
+    private ?int $rate = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $posted_date = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(string $comment): static
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    public function getRate(): ?int
+    {
+        return $this->rate;
+    }
+
+    public function setRate(int $rate): static
+    {
+        $this->rate = $rate;
+
+        return $this;
+    }
+
+    public function getPostedDate(): ?\DateTimeInterface
+    {
+        return $this->posted_date;
+    }
+
+    public function setPostedDate(\DateTimeInterface $posted_date): static
+    {
+        $this->posted_date = $posted_date;
+
+        return $this;
+    }
+}
